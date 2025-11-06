c55a0ec7a985e45152b3f239de448149e19167c6
ishitasaxena2614 <ishitasaxena324@gmail.com>
2025-11-06 13:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function stringChop(str, size) {
-  // your code here
-}
+   // If input string is null or empty, return empty array
+  if (!str) return [];
+
+  const result = [];
+  // Convert size to number (as prompt returns string)
+  size = Number(size);
+
+  // Loop through the string in steps of 'size'
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+
+  return result;
+}  
 
 // Do not change the code below
 const str = prompt("Enter String.");
